1c692926c7971c1ae4293de53b303930447a27b6
Keith Wellman <keith.wellman@icloud.com>
2016-11-03 14:29:21 -0400

    Less bugs, working puzzle

--- fifteen.js
+++ fifteen.js
@@ -1,3 +1,7 @@
+// Extra features implemented:
+// End of game notification - uses the explode effect
+//
+
 $("document").ready(function() {
   // on mouseenter check if it's a movable piece
   $(".piece").on('mouseenter', this,
@@ -9,7 +13,7 @@ $("document").ready(function() {
   $(".piece").on('mouseleave', this,
     function (event) {
       if (event.target.id.toString() != "square16") {
-        $('#'+event.target.id.toString()).css({"border-color": "black", "color": "white", "text-decoration": "none"});
+        $(document.getElementById(event.target.id)).css({"border-color": "black", "color": "white", "text-decoration": "none"});
       }
     }
   );
@@ -19,19 +23,44 @@ $("document").ready(function() {
 });
 
 var god = [];
+var selectBg;
+var selectedBg;
+var bg;
+function setBg() {
+  selectBg = document.getElementById("bgSelect");
+  selectedBg = selectBg.value;
+  if (selectedBg == "HAL") {
+    bg = "background.jpg";
+  }
+  else if (selectedBg == "cs") {
+    bg = "cs.jpg";
+  }
+  else if (selectedBg == "Always Sunny") {
+    bg = "sunny.jpg";
+  }
+  else if (selectedBg == "enjoy") {
+    bg = "enjoy.png";
+  }
+}
+
 
 // draw the squares: numOfRows^2
 function drawBoard(numOfRows) {
   board = [];
   boardObj = [];
+
   for (var i = 0; i < (numOfRows*numOfRows); i++) {
+    setBg();
     var squareDiv = document.createElement("div");
     squareDiv.id = "square" + (i+1); // give each square a unique id
     squareDiv.className = "piece";
     if (i < ((numOfRows*numOfRows)-1)) {
-      squareDiv.innerHTML = "<span>"+(i+1)+"</span>"; // display the number on the square
+      squareDiv.innerHTML = "<p>"+(i+1)+"</p>"; // display the number on the square
     }
     document.getElementById("squares").appendChild(squareDiv);
+    if (squareDiv.id != "square16") {
+      document.getElementById(squareDiv.id).style.backgroundImage = "url('"+bg+"')";
+    }
     board[i] = squareDiv.id; //store each square id in an array
     boardObj[i] = squareDiv; //store each square object in array
   }
@@ -40,27 +69,34 @@ function drawBoard(numOfRows) {
 function shuffleBoard() {
   //shuffle the board
   for (var i = 0; i < 20; i++) {
-    var randomNum = Math.floor(Math.random() * 16) + 1;
-    // var godCode =
-    move("square"+randomNum);
-    // if (godCode != -1) {
-    //   god.push(godCode.pieceIndex); // creates an array of moves
-    // }
+    var randomNum = Math.floor(Math.random() * 15) + 1;
+    var godCode = move("square"+randomNum);
+    if (godCode != -1) {
+      god.push(godCode.pieceIndex); // creates an array of moves
+      // document.getElementById("cheat").innerHTML += "square"+randomNum+"\n";
+    }
   }
-  // alert(god);
   // clear style after shuffle
   jQuery(function($) {
     for (var i = 1; i < 16; i++) {
-      $("#square"+i).css({"border-color": "black", "color": "white", "text-decoration": "none"});
+      var squ = "#square"+i;
+      $(squ).css({"border-color": "black", "color": "white", "text-decoration": "none"});
     }
   });
 }
-
+function swapElements(obj1, obj2) {
+    var temp = document.createElement("div");
+    obj1.parentNode.insertBefore(temp, obj1); //inert tmp before obj1
+    obj2.parentNode.insertBefore(obj1, obj2); //insert obj1 before obj2
+    temp.parentNode.insertBefore(obj2, temp); //insert obj2 before temp
+    temp.parentNode.removeChild(temp); //remove temp
+    winner();
+}
 // move piece to empty space
 function move(piece) {
   // check if next to empty square and then move it
   var pieceObj = highlight(piece);
-  if(pieceObj >= 0 || pieceObj.pieceIndex >= 1) {
+  if(pieceObj >= 0 || pieceObj.pieceIndex >= 0) {
     // swap the empty piece with the clicked piece in the board and boardObj arrays
     var tmp = board[pieceObj.pieceIndex];
     var tmpObjPiece = boardObj[pieceObj.pieceIndex];
@@ -69,34 +105,15 @@ function move(piece) {
     boardObj[pieceObj.pieceIndex] = boardObj[pieceObj.emptyPieceIndex];
     board[pieceObj.emptyPieceIndex] = tmp;
     boardObj[pieceObj.emptyPieceIndex] = tmpObjPiece;
-    jQuery(function ($) {
-      // swap the pieces
-      // detach the empty piece, insert it before the clicked piece, then put the
-      // empty piece after the piece that was next to the empty piece
-      var detachedPiece = $(tmpObjPiece).detach();
-      $(detachedPiece).insertAfter(tmpObjEmpty);
-      $(tmpObjEmpty).insertAfter(boardObj[pieceObj.pieceIndex - 1]);
-    });
-    return pieceObj;
-  }
-  //handle the edge case of the top left position being clicked
-  else if (pieceObj.pieceIndex == 0) {
-    // swap the empty piece with the clicked piece in the board and boardObj arrays
-    var tmp = board[pieceObj.pieceIndex];
-    var tmpObjPiece = boardObj[pieceObj.pieceIndex];
-    var tmpObjEmpty = boardObj[pieceObj.emptyPieceIndex];
-    board[pieceObj.pieceIndex] = board[pieceObj.emptyPieceIndex];
-    boardObj[pieceObj.pieceIndex] = boardObj[pieceObj.emptyPieceIndex];
-    board[pieceObj.emptyPieceIndex] = tmp;
-    boardObj[pieceObj.emptyPieceIndex] = tmpObjPiece;
-    jQuery(function ($) {
-      // swap the pieces
-      // detach the empty piece, insert it before the clicked piece, then put the
-      // empty piece after the piece that was next to the empty piece
-      var detachedPiece = $(tmpObjPiece).detach();
-      $(detachedPiece).insertAfter(tmpObjEmpty);
-      $(tmpObjEmpty).insertBefore(boardObj[pieceObj.pieceIndex + 1]);
-    });
+    // jQuery(function ($) {
+    //   // swap the pieces
+    //   // detach the empty piece, insert it before the clicked piece, then put the
+    //   // empty piece after the piece that was next to the empty piece
+    //   var detachedPiece = $(tmpObjPiece).detach();
+    //   $(detachedPiece).insertAfter(tmpObjEmpty);
+    //   $(tmpObjEmpty).insertAfter(boardObj[pieceObj.pieceIndex - 1]);
+    // });
+    swapElements(tmpObjPiece, tmpObjEmpty);
     return pieceObj;
   }
   return -1;
@@ -109,30 +126,30 @@ function highlight(piece) {
   var pieceInfo = {pieceIndex: "", emptyPieceIndex: -1, thepiece: piece};
 
   // piece should highlight if:
-  // index +1 is square16
-  // index -1 is square16
+  // index +1 is square16 - unless it's in the next row postion 3, 7, or 11
+  // index -1 is square16 - unless it's in the previous row position 12, 8, or 4
   // index +4 is square16
   // index -4 is square16
 
   // verify array position exists first and then check if the piece is next to the empty space
   // returns the index of empty space
   if(pieceIndex + 1 <= boardSize) {
-    if (board[pieceIndex + 1] == "square16") {
-      //jquery hover: change appearence of piece that can be moved
-      jQuery(function($) {
-        $('#'+piece).css({"border-color": "red", "color": "#006600", "text-decoration": "underline"});
-      });
+    if (board[pieceIndex + 1] == "square16" && pieceIndex != 3 && pieceIndex != 7 && pieceIndex !=11) {
+      //change appearence of piece that can be moved
+      document.getElementById(piece).style.borderColor = "red";
+      document.getElementById(piece).style.color = "#006600";
+      document.getElementById(piece).style.textDecoration = "underline";
       pieceInfo.pieceIndex = pieceIndex;
       pieceInfo.emptyPieceIndex = pieceIndex + 1;
       return pieceInfo;
     }
   }
   if(pieceIndex - 1 >= 0) {
-    if (board[pieceIndex - 1] == "square16") {
-      // jquery hover: change appearence of piece that can be moved
-      jQuery(function($) {
-        $('#'+piece).css({"border-color": "red", "color": "#006600", "text-decoration": "underline"});
-      });// end jquery hover
+    if (board[pieceIndex - 1] == "square16" && pieceIndex != 12 && pieceIndex != 8 && pieceIndex !=4) {
+      //change appearence of piece that can be moved
+      document.getElementById(piece).style.borderColor = "red";
+      document.getElementById(piece).style.color = "#006600";
+      document.getElementById(piece).style.textDecoration = "underline";
       pieceInfo.pieceIndex = pieceIndex;
       pieceInfo.emptyPieceIndex = pieceIndex - 1;
       return pieceInfo;
@@ -140,10 +157,10 @@ function highlight(piece) {
   }
   if (pieceIndex + 4 <= boardSize) {
     if (board[pieceIndex + 4] == "square16") {
-      // jquery hover: change appearence of piece that can be moved
-      jQuery(function($) {
-        $('#'+piece).css({"border-color": "red", "color": "#006600", "text-decoration": "underline"});
-      });// end jquery hover
+      //change appearence of piece that can be moved
+      document.getElementById(piece).style.borderColor = "red";
+      document.getElementById(piece).style.color = "#006600";
+      document.getElementById(piece).style.textDecoration = "underline";
       pieceInfo.pieceIndex = pieceIndex;
       pieceInfo.emptyPieceIndex = pieceIndex + 4;
       return pieceInfo;
@@ -151,14 +168,32 @@ function highlight(piece) {
   }
   if (pieceIndex - 4 >= 0) {
     if (board[pieceIndex - 4] == "square16") {
-      // jquery hover: change appearence of piece that can be moved
-      jQuery(function($) {
-        $('#'+piece).css({"border-color": "red", "color": "#006600", "text-decoration": "underline"});
-      });// end jquery hover
+      //change appearence of piece that can be moved
+      document.getElementById(piece).style.borderColor = "red";
+      document.getElementById(piece).style.color = "#006600";
+      document.getElementById(piece).style.textDecoration = "underline";
       pieceInfo.pieceIndex = pieceIndex;
       pieceInfo.emptyPieceIndex = pieceIndex - 4;
       return pieceInfo;
     }
   }
   return -1;
+}
+
+function winner() {
+  var win = true;
+  for (var i = 0; i < board.length; i++) {
+    var x = i+1;
+    if (board[i].toString() != "square"+x) {
+      win = false;
+    }
+  }
+  if (win) {
+    jQuery(function ($) {
+      $(".puzzle").hide("explode", 1000, function() {
+        alert("you won!");
+        window.location.reload();
+      });
+    });
+  }
 }
